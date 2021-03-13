520aef8f3ccb22abb0bce93d911385522c670fb9
cybersi-io <66743889+cybersi-io@users.noreply.github.com>
2021-03-13 19:06:15 +0000

    Added environmental variables

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
 import os
 import discogs_client
-
-d = discogs_client.Client('RecordDataFinder/0.1', user_token="ebrELjhwzwbrWOcmGapdIrSpFRAyFwDMXrvgydzF")
+print('USER_TOKEN' in os.environ)
+d = discogs_client.Client('RecordDataFinder/0.1', user_token=os.environ['USER_TOKEN'])
 
 results = d.search('Physical Graffiti', type='release')
-print(dir(results[0].artists[0]))
+#print(dir(results[0].artists[0]))
 print(results[0].artists[0])
 print(results[0].labels[0])
 print(results[0].formats[0])
@@ -15,3 +15,6 @@ print(results[0].data['num_for_sale'])
 print(results[0].data['lowest_price'])
 print(results[0].data['year'])
 print(results[0].data['country'])
+
+#for y in results:
+#    print(y)
