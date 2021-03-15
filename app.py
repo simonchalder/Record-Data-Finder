742545ce37008b52834aef776eddf97faded2007
cybersi-io <66743889+cybersi-io@users.noreply.github.com>
2021-03-15 13:45:31 +0000

    Very basic GUI created with artist search working

--- app.py
+++ app.py
@@ -3,76 +3,123 @@
 from dotenv import load_dotenv
 import os
 import discogs_client
+from tkinter import *
 
-# API Call
+root = Tk() # Create the window widget
+root.title("Record Data Finder")
+
 d = discogs_client.Client('RecordDataFinder/0.1', user_token=os.environ['USER_TOKEN'])
 
-#Get user input for artist
-search_term = input("Enter Artist Name: ")
+def clicked():
+    results = d.search(artist_input.get(),type='artist')
+    artist_id = results[0].id
+    artist = d.artist(artist_id)
+    i = 1
+    for x in artist.releases:
+        releases_box.insert(i, x)
+        
 
-#Create a results object which contains the results of the artist search
-results = d.search(search_term,type='artist')
 
-#Createa new variable 'artist_id' and assign it the first result from the artist search
-artist_id = results[0].id
 
-#Create an 'artist' object which contains all data about the artist
-artist = d.artist(artist_id)
 
-#Loop through the artist releases and print to the console
-for y in artist.releases:
-    print(y)
- #User input for the requested release
-release_search = input("Enter Release Code: ")
 
-#Create a record object with the results of the release search
-record = d.release(release_search)
 
-#Variable creation for release search results
-title = record.title
-tracklist = record.tracklist
-url = record.url
-formats = record.formats
 
-#More variables with exception handling as they may not exist in the search results
-try:
-    num_for_sale = record.data['num_for_sale']
-except Exception:
-    num_for_sale = "N/A"
+artist_input = Entry(root, width=50)
+artist_input.pack()
 
-try:
-    lowest_price = record.data['lowest_price']
-except Exception:
-    lowest_price = "N/A"
+artist_button = Button(root, text="Submit", padx=20, pady=10, command=clicked)
+artist_button.pack()
 
-try:
-    year = record.data['year']
-except Exception:
-    year = "N/A"
+releases_box = Listbox(root, xscrollcommand=True, yscrollcommand=True)
+releases_box.pack()
 
-try:
-    country = record.data['country']
-except Exception:
-    country = "N/A"
 
-try:
-    catno = record.data['catno']
-except Exception:
-    catno = "N/A"
 
-#Print final data to the console
 
-print("\n******** Record Data Finder **********\n")
-print("Title: " + title)
-print("Discogs URL: " + url)
-print("Formats: ")
-for x in formats:
-    print(x)
-print("Tracklist:")
-for x in tracklist:
-    print(x)
-print("Year: " + str(year))
-print("Country: " + country)
-print("Catalogue No: " + str(catno))
-print("Number for sale: " + str(num_for_sale))
-print("Lowest price ($): " + str(lowest_price))
+
+
+
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
+# # API Call
+# d = discogs_client.Client('RecordDataFinder/0.1', user_token=os.environ['USER_TOKEN'])
+
+# #Get user input for artist
+# search_term = input("Enter Artist Name: ")
+
+# #Create a results object which contains the results of the artist search
+# results = d.search(search_term,type='artist')
+
+# #Createa new variable 'artist_id' and assign it the first result from the artist search
+# artist_id = results[0].id
+
+# #Create an 'artist' object which contains all data about the artist
+# artist = d.artist(artist_id)
+
+# #Loop through the artist releases and print to the console
+# for y in artist.releases:
+#     print(y)
+#  #User input for the requested release
+# release_search = input("Enter Release Code: ")
+
+# #Create a record object with the results of the release search
+# record = d.release(release_search)
+
+# #Variable creation for release search results
+# title = record.title
+# tracklist = record.tracklist
+# url = record.url
+# formats = record.formats
+
+# #More variables with exception handling as they may not exist in the search results
+# try:
+#     num_for_sale = record.data['num_for_sale']
+# except Exception:
+#     num_for_sale = "N/A"
+
+# try:
+#     lowest_price = record.data['lowest_price']
+# except Exception:
+#     lowest_price = "N/A"
+
+# try:
+#     year = record.data['year']
+# except Exception:
+#     year = "N/A"
+
+# try:
+#     country = record.data['country']
+# except Exception:
+#     country = "N/A"
+
+# try:
+#     catno = record.data['catno']
+# except Exception:
+#     catno = "N/A"
+
+# #Print final data to the console
+
+# print("\n******** Record Data Finder **********\n")
+# print("Title: " + title)
+# print("Discogs URL: " + url)
+# print("Formats: ")
+# for x in formats:
+#     print(x)
+# print("Tracklist:")
+# for x in tracklist:
+#     print(x)
+# print("Year: " + str(year))
+# print("Country: " + country)
+# print("Catalogue No: " + str(catno))
+# print("Number for sale: " + str(num_for_sale))
+# print("Lowest price ($): " + str(lowest_price))
